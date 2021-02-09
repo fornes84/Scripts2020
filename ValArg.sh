4ed0216ce1d3e17452663369338c46c402108680
marc <marc@localhost.localdomain>
2021-02-09 11:17:30 +0100

    aaaa2

---
+++ ValArg.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+# SO. ISIX1  09/02/21
+#
+# Validar si hi ha exactament 2 arguments i mostrar-los
+
+# Validació errors
+
+if [ $# -ne 2 ]
+then
+  echo "Error numero arguments error"
+  echo "Usage $0: arg1 arg2"
+exit 1	
+
+# XIXA
+
+echo " Arguments correctes $1 i $2 "
+
+exit 0
